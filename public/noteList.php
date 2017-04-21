af2f13f6e27610b66b5718c6d4dd8b329ae3befc
Andrew Rivett <veggiefrog@gmail.com>
2017-04-21 10:00:52 -0400

    Added archive button for notes

--- public/noteList.php
+++ public/noteList.php
@@ -21,6 +21,7 @@ function ciniki_poma_noteList($ciniki) {
     $rc = ciniki_core_prepareArgs($ciniki, 'no', array(
         'business_id'=>array('required'=>'yes', 'blank'=>'no', 'name'=>'Business'),
         'customer_id'=>array('required'=>'no', 'blank'=>'yes', 'name'=>'Customer ID'),
+        'archive_note_id'=>array('required'=>'no', 'blank'=>'yes', 'name'=>'Archive Note ID'),
         'customers'=>array('required'=>'no', 'blank'=>'yes', 'name'=>'Customers'),
         ));
     if( $rc['stat'] != 'ok' ) {
@@ -49,6 +50,28 @@ function ciniki_poma_noteList($ciniki) {
         return $rc;
     }
     $maps = $rc['maps'];
+
+    //
+    // Check if note to be archived
+    //
+    if( isset($args['archive_note_id']) && $args['archive_note_id'] > 0 ) {
+        $strsql = "SELECT id, status "
+            . "FROM ciniki_poma_notes "
+            . "WHERE id = '" . ciniki_core_dbQuote($ciniki, $args['archive_note_id']) . "' "
+            . "AND ciniki_poma_notes.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+            . "";
+        $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.poma', 'note');
+        if( $rc['stat'] != 'ok' ) {
+            return $rc;
+        }
+        if( isset($rc['note']) && $rc['note']['status'] == 10 ) {
+            ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'objectUpdate');
+            $rc = ciniki_core_objectUpdate($ciniki, $args['business_id'], 'ciniki.poma.note', $args['archive_note_id'], array('status'=>60), 0x07);
+            if( $rc['stat'] != 'ok' ) {
+                return $rc;
+            }
+        }
+    }
 
     //
     // Get the list of notes which are active
@@ -135,6 +158,7 @@ function ciniki_poma_noteList($ciniki) {
     //
     if( isset($args['customers']) && $args['customers'] == 'yes' ) {
         $strsql = "SELECT ciniki_poma_notes.customer_id, "
+            . "ciniki_poma_notes.status, "
             . "IFNULL(ciniki_customers.display_name, 'General Notes') AS display_name, "
             . "COUNT(ciniki_poma_notes.id) AS num_items "
             . "FROM ciniki_poma_notes "
@@ -143,11 +167,12 @@ function ciniki_poma_noteList($ciniki) {
                 . "AND ciniki_customers.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
                 . ") "
             . "WHERE ciniki_poma_notes.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
-            . "GROUP BY customer_id "
+            . "GROUP BY customer_id, status "
             . "ORDER BY ciniki_customers.display_name "
             . "";
         $rc = ciniki_core_dbHashQueryArrayTree($ciniki, $strsql, 'ciniki.poma', array(
-            array('container'=>'customers', 'fname'=>'customer_id', 'fields'=>array('id'=>'customer_id', 'display_name', 'num_items')),
+            array('container'=>'customers', 'fname'=>'customer_id', 'fields'=>array('id'=>'customer_id', 'display_name')),
+            array('container'=>'statuses', 'fname'=>'status', 'fields'=>array('status', 'num_items')),
             ));
         if( $rc['stat'] != 'ok' ) { 
             return $rc;
@@ -156,6 +181,16 @@ function ciniki_poma_noteList($ciniki) {
             $rsp['customers'] = array();
         } else {
             $rsp['customers'] = $rc['customers'];
+            foreach($rsp['customers'] as $cid => $customer) {
+                $rsp['customers'][$cid]['num_items'] = 0;
+                if( isset($customer['statuses']) ) {
+                    foreach($customer['statuses'] as $status) {
+                        if( $status['status'] == 10 ) {
+                            $rsp['customers'][$cid]['num_items'] = $status['num_items'];
+                        }
+                    }
+                }
+            }
         }
     }
 
