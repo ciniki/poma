3c2f676860c2be42e68b34ae0dc0d302f9e5c522
Andrew Rivett <veggiefrog@gmail.com>
2020-05-04 20:21:51 -0400

    Fixed bug with choosing pickup time on closed orders

--- web/processRequestClosed.php
+++ web/processRequestClosed.php
@@ -68,6 +68,8 @@ function ciniki_poma_web_processRequestClosed(&$ciniki, $settings, $tnid, $args)
         return array('stat'=>'500', 'err'=>array('code'=>'ciniki.poma.225', 'msg'=>'Unable to find order dates'));
     }
     $date_id = 0;
+    $cur_dt = new DateTime('now', new DateTimezone($intl_timezone));
+    $cur_order_dt = null;
     if( !isset($rc['dates']) || count($rc['dates']) == 0 ) {
         $page['blocks'][] = array('type'=>'formmessage', 'level'=>'error', 'message'=>"Oops, it looks like we forgot to add more available dates. Please contact us and we'll get more dates added.");
     } else {
@@ -80,8 +82,12 @@ function ciniki_poma_web_processRequestClosed(&$ciniki, $settings, $tnid, $args)
             }
             $odate['order_dt'] = new DateTime($odate['order_date'] . ' 12:00:00', new DateTimezone($intl_timezone));
             $odate['display_name'] = $odate['order_dt']->format("M jS, Y");
+            if( $cur_order_dt == null ) {
+                $cur_order_dt = $odate['order_dt'];
+            }
             if( isset($_GET['d']) && $_GET['d'] > 0 && $_GET['d'] == $odate['order_date'] ) {
                 $date_id = $odate['id'];
+                $cur_order_dt = $odate['order_dt'];
                 $odate['order_date_text'] = $odate['order_dt']->format('M d, Y');
                 $content .= "<option value='" . $odate['order_date'] . "' selected>" . $odate['display_name'] . "</option>";
             } else {
@@ -111,14 +117,104 @@ function ciniki_poma_web_processRequestClosed(&$ciniki, $settings, $tnid, $args)
         } else {
             $order = $rc['order'];
             
-            if( isset($rc['order']['items']) && count($rc['order']['items']) > 0 ) {
-                $page['blocks'][] = array('type'=>'orderdetails', 'section'=>'order-details', 'size'=>'wide', 
-                    'title'=>$rc['order']['order_date_text'], 
-                    'base_url'=>$args['base_url'],
-                    'order'=>$rc['order']);
-            } else {
-                $page['blocks'][] = array('type'=>'message', 'title'=>'Order', 'size'=>'wide', 'level'=>'warning', 
-                    'content'=>"Your order is currently empty.");
+            //
+            // Check if request to change pickup time
+            //
+            if( isset($args['uri_split'][0]) && $args['uri_split'][0] == 'pickuptime' ) {
+                //
+                // Get the list of existing taken timeslots
+                //
+                $strsql = "SELECT id, pickup_time " 
+                    . "FROM ciniki_poma_orders "
+                    . "WHERE date_id = '" . ciniki_core_dbQuote($ciniki, $date_id) . "' "
+                    . "AND tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+                    . "";
+                ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryIDTree');
+                $rc = ciniki_core_dbHashQueryIDTree($ciniki, $strsql, 'ciniki.poma', array(
+                    array('container'=>'times', 'fname'=>'pickup_time', 'fields'=>array('pickup_time')),
+                    array('container'=>'orders', 'fname'=>'id', 'fields'=>array('id')),
+                    ));
+                if( $rc['stat'] != 'ok' ) {
+                    return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.poma.217', 'msg'=>'Unable to load pickup times', 'err'=>$rc['err']));
+                }
+                $picked_times = isset($rc['times']) ? $rc['times'] : array();
+                if( isset($_GET['t']) && $_GET['t'] != '' ) {
+                    $new_dt = new DateTime($cur_order_dt->format('Y-m-d') . ' ' . $_GET['t'], new DateTimezone($intl_timezone));
+                    if( isset($picked_times[$new_dt->format('g:i a')]) ) {
+                        $page['blocks'][] = array('type'=>'formmessage', 'level'=>'error', 'message'=>'That time is unavailable');
+                    } else {
+                        //
+                        // Update the order
+                        //
+                        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'objectUpdate');
+                        $rc = ciniki_core_objectUpdate($ciniki, $tnid, 'ciniki.poma.order', $order['id'], array('pickup_time'=>$new_dt->format('g:i a')), 0x04);
+                        if( $rc['stat'] != 'ok' ) {
+                            $page['blocks'][] = array('type'=>'formmessage', 'level'=>'error', 'message'=>'Unable to update order');
+                        } else {
+                            header("Location: " . $args['base_url'] . '/closed?d=' . $cur_order_dt->format('Y-m-d'));
+                            exit;
+                        }
+                    }
+                }
+
+                //
+                // List the available times
+                //
+                $start_dt = new DateTime($ciniki['session']['ciniki.poma']['date']['pickupstart_dt'], new DateTimezone('UTC'));
+                $start_dt->setTimezone(new DateTimezone($intl_timezone));
+                $end_dt = new DateTime($ciniki['session']['ciniki.poma']['date']['pickupend_dt'], new DateTimezone('UTC'));
+                $end_dt->setTimezone(new DateTimezone($intl_timezone));
+                $interval = new DateInterval('PT5M');
+                $options = array();
+                while($start_dt < $end_dt) {
+                    $option = array(
+                        'label' => $start_dt->format('g:i a'),
+                        'url' => $args['base_url'] . '/closed/pickuptime?d=' . $cur_order_dt->format('Y-m-d') . '&t=' . $start_dt->format('H:i'), 
+                        );
+                    if( !isset($picked_times[$start_dt->format('g:i a')]) ) {
+                        $option['available'] = 'yes';
+                    } elseif( $order['pickup_time'] == $start_dt->format('g:i a') ) {
+                        $option['message'] = '<b>Current</b>';
+                    } else {
+                        $option['message'] = 'Unavailable';
+                    }
+                    $options[] = $option;
+                    // Move to next time slot
+                    $start_dt->add($interval);
+                }
+                $page['blocks'][] = array('type' => 'optionlist', 
+                    'title' => 'Choose your pickup time', 
+                    'wide' => 'yes', 
+                    'button-label' => 'Select',
+                    'base_url' => $args['base_url'], 
+                    'options' => $options,
+                    );
+            } 
+            //
+            // Output the order details
+            //
+            else {
+                // Make sure order date is in the future, can't change pickup time of past orders
+                $cur_order_dt->setTime(23, 59, 59);
+                $cur_dt->add(new DateInterval('P2D'));
+                if( ciniki_core_checkModuleFlags($ciniki, 'ciniki.poma', 0x08) && $order['id'] > 0 && $cur_dt <= $cur_order_dt ) {
+                    if( $order['pickup_time'] == '' ) {
+                        $content = "<br/><b>Please <a href='{$args['base_url']}/closed/pickuptime?d=" . $cur_order_dt->format('Y-m-d') . "'>Choose A Pickup Time</a> for your order.</b>";
+                    } else {
+                        $content = "<br/><b>Your Pickup Time: " . $order['pickup_time'] . "</b> <a href='{$args['base_url']}/closed/pickuptime?d=" . $cur_order_dt->format('Y-m-d') . "'>Change Time</a>";
+                    }
+                    $page['blocks'][] = array('type'=>'content', 'section'=>'order-date', 'html'=>$content);
+                }
+
+                if( isset($rc['order']['items']) && count($rc['order']['items']) > 0 ) {
+                    $page['blocks'][] = array('type'=>'orderdetails', 'section'=>'order-details', 'size'=>'wide', 
+                        'title'=>$rc['order']['order_date_text'], 
+                        'base_url'=>$args['base_url'],
+                        'order'=>$rc['order']);
+                } else {
+                    $page['blocks'][] = array('type'=>'message', 'title'=>'Order', 'size'=>'wide', 'level'=>'warning', 
+                        'content'=>"Your order is currently empty.");
+                }
             }
         }
     }
